7a5b9843eab62895935ceec4fa3f19f3d73b6139
chriskim511 <christopherkim511@gmail.com>
2014-03-24 22:19:16 -0400

    Added HW10 with Tail

---
+++ 10_linkedlist3/Node.java
@@ -0,0 +1,52 @@
+public class Node {
+    
+    private String data;
+    private Node next;
+    
+    public Node(String d) {
+	data = d;
+    }
+    public void setData(String d) {
+	data = d;
+    }
+    public String getData() {
+	return data;
+    }
+    public void setNext(Node n) {
+	next = n;
+    }
+    public Node getNext() {
+	return next;
+    }
+    public String toString() {
+	return data;
+    }
+    public static void main(String[] args) {
+	Node n1 = new Node("Sully");
+	Node n2 = new Node("Mike");
+	System.out.println(n1);
+	System.out.println(n2);
+
+	n1.setNext(n2);
+	System.out.println(n1.getNext());
+	n1.getNext().setNext( new Node("Randall") );
+	System.out.println();
+	System.out.println(n2.getNext());
+	System.out.println(n1.getNext().getNext());
+	System.out.println();
+	/*
+	  n1.setNext(n1.getNext().getNext() );
+	  System.out.println();
+	  System.out.println(n1);
+	  System.out.println(n1.getNext());
+	  System.out.println(n1.getNext().getNext());
+	*/
+	n1.getNext().getNext().setNext(n1);
+	System.out.println(n1);
+	System.out.println(n1.getNext());
+	System.out.println(n1.getNext().getNext());
+
+	
+
+    }
+}
